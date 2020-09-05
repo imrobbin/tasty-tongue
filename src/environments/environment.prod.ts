2c9abd9e01fd49c168a9bc4e3b2d0d2b64e40d73
Ravindra Patle <me24-git@users.github.com>
2020-09-05 16:11:04 +0530

    created git repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyCZGf1vOhCve-nPuKj6f_8TuUJKmKz7y5g',
+        authDomain: 'tasty-tongue-res.firebaseapp.com',
+        databaseURL: 'https://tasty-tongue-res.firebaseio.com',
+        projectId: 'tasty-tongue-res',
+        storageBucket: 'tasty-tongue-res.appspot.com',
+        messagingSenderId: '317010414900',
+        appId: '1:317010414900:web:791d2a01bf47712cdaa961',
+        measurementId: 'G-1QD4F4BRW8',
+        vapidKey:
+            'BF4LZg-4xng3mFhazZ9vZik86L1MGKLn-kuKC5xhhM6p0-6LFWw-FmEsc88_KUMAYFrc8-7KzqF5VZy5ZJypR8w',
+    },
+    defaultImageBanner: '../../../assets/images/default-img-banner.gif',
+    defaultImageThumb: '../../../assets/images/default-img-thumb.gif',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyCZGf1vOhCve-nPuKj6f_8TuUJKmKz7y5g',
+        authDomain: 'tasty-tongue-res.firebaseapp.com',
+        databaseURL: 'https://tasty-tongue-res.firebaseio.com',
+        projectId: 'tasty-tongue-res',
+        storageBucket: 'tasty-tongue-res.appspot.com',
+        messagingSenderId: '317010414900',
+        appId: '1:317010414900:web:791d2a01bf47712cdaa961',
+        measurementId: 'G-1QD4F4BRW8',
+        vapidKey:
+            'BF4LZg-4xng3mFhazZ9vZik86L1MGKLn-kuKC5xhhM6p0-6LFWw-FmEsc88_KUMAYFrc8-7KzqF5VZy5ZJypR8w',
+    },
+    defaultImageBanner: '../../../assets/images/default-img-banner.gif',
+    defaultImageThumb: '../../../assets/images/default-img-thumb.gif',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
